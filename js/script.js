6adcc42215847caadcf41202fb5036d95d3351d6
Milan Nain <70199314+nainmilan189@users.noreply.github.com>
2025-04-16 17:18:13 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('form')?.addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thanks for contacting Umda Utilities! We will get back to you soon.');
+  });
+  
